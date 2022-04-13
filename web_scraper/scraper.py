e065b60a3d824dd475517a13c041e8cbbd955a39
dialaabulkhail <diala.sh.98@gmail.com>
2022-04-13 11:23:10 +0300

    wikipedia scraping

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -5,10 +5,8 @@ import json
 
 url = "https://en.wikipedia.org/wiki/History_of_Mexico"
 
-"""
-This function will return an integer of number of passages that need citation in the url.
-"""
-def get_citations_needed_count(url):
+
+def fixed(url):
     response = requests.get(url)
 
     soup = BeautifulSoup(response.content, "html.parser")
@@ -21,7 +19,16 @@ def get_citations_needed_count(url):
         if passage not in cited_passages:
             uncited_list.append(passage.get_text())
 
-    uncited_passages = len(uncited_list)
+    return uncited_list
+
+
+"""
+This function will return an integer of number of passages that need citation in the url.
+"""
+def get_citations_needed_count(url):
+    data = fixed(url)
+
+    uncited_passages = len(data)
 
     return uncited_passages
 
@@ -31,28 +38,21 @@ def get_citations_needed_count(url):
 This function will return a formatted string of each uncited passage in the right order.
 """
 def get_citations_needed_report(url):
-    response = requests.get(url)
+    data = fixed(url)
 
-    soup = BeautifulSoup(response.content, "html.parser")
-    all_passages = soup.find("p")  
+    # array = []
+    for i in data:
+        print(f"citation is needed for: '{i}' passage")
+        print("_"*50)
 
-    cited_passages = all_passages.find_all("sup", class_ = "reference")  
+ 
 
-    uncited_list = []
-    for passage in all_passages:
-        if passage not in cited_passages:
-            uncited_list.append(passage.get_text())
-
-    array = []
-    for i in uncited_list:
-        array.append(f"citation needed for '{i}' passage")
-
-    with open("uncited_passages.json", "w") as f:
-        content = json.dumps(array)
-        f.write(content)
+    # with open("uncited_passages.json", "w") as f:
+    #     content = json.dumps(array)
+    #     f.write(content)
     
 
 
 if __name__ == "__main__":
-    # print(get_citations_needed_count(url))
-    print(get_citations_needed_report(url))
+    print(get_citations_needed_count(url))
+    get_citations_needed_report(url)
